3f32aeaa5d910bafff10e8805f2deebcd2c78b18
nguyenminhduc2c19 <nguyenminhduc2c19@gmail.com>
2024-01-21 01:08:24 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Topmenu } from "./header/top-menu/Topmenu"
+import "./App.css"
+
+export default function App(){
+  return (
+    <div className="App">
+  <div className="header">
+    <Topmenu />
+  </div>
+  </div>
+  )
+}
